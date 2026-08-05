b6e01a67f11ab9c4e24b37659a9fcf5c76e148ef
Arthur <arthurhleocadio@gmail.com>
2026-08-05 20:36:10 -0300

    Create imc.cpp
    TERMINAR DEPOIS A TAREFA DE IMC PARA ENTREGAR

---
+++ imc.cpp
@@ -0,0 +1,58 @@
+#include <string>
+using namespace std;
+
+class IMC
+{
+    private: float peso,altura,vIMC;
+    
+    public: void setValores(float p,float a)
+    {
+        peso = p;
+        altura = a;
+    };
+    public: float calcular()
+    {
+        vIMC = (peso / (altura * altura));
+        return vIMC;
+    };
+    public: float getIMC()
+    {
+      return vIMC;  
+    };
+     public: float getPeso()
+    {
+        return peso;
+    }
+    public: float getAltura()
+    {
+        return altura;
+    }
+};
+int main()
+{
+    float p,a,imc;
+    
+    IMC vimc;
+    printf("\nREALIZANDO O CALCULO DE IMC");
+    printf("\nDigite o seu peso:  ");
+    scanf("%f",&p);
+    printf("\nDigite a sua altura:  ");
+    scanf("%f",&a);
+    vimc.setValores(p,a);
+    vimc.calcular();
+    printf("\nO IMC É = %7.2f",vimc.getIMC());
+    
+    if(vimc.getIMC()<=18.5)
+    {
+        printf("VOCÊ ESTÁ ABAIXO DO PESO ");
+    }
+    else
+    {
+        printf("TESTE");
+    }
+    
+    
+    
+    
+    
+}
