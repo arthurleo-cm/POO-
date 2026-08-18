ccdbd29d2e5b63fe9d50ccad14295dd3ddaa432b
Arthur <arthurhleocadio@gmail.com>
2026-08-18 20:42:56 -0300

    Update Inteiros.cpp
    Primeira versão do código 
    falta mais ainda

--- Inteiros.cpp
+++ Inteiros.cpp
@@ -1,1 +1,62 @@
+#include <stdio.h>
+/*
 
+Exercício de inteiros
+
+1)  Informar o número  de dígitos
+2) somar todos os valores  dos dígitos
+3)  multiplicar todos os valores dos dígitos
+4)  indicar o numero de dígitos pares e o número de dígitos impares
+
+*/
+
+
+class INTEIROS
+{
+    private: int digitos,resultado,numero;
+    
+    public: void setNumero(int n)
+    {
+        numero = n;
+        
+        
+    };
+    
+    public: int getNumero()
+    {
+     
+        return numero;
+    }
+    
+    public: int NumeroDigitos(int numero)
+    {
+       int contador = 0;
+        
+       do {
+            numero = numero/10;
+            contador++;
+            
+            
+            
+        }  while  (contador != 0);
+        
+        
+       return contador;
+       
+    }
+    public: int getNumeroDigitos()
+    {
+        
+        return digitos;
+        
+    };
+  
+    
+};
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
