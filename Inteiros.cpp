92f0ff8ddb102caa19ef5aff18edee24649f878b
Arthur <arthurhleocadio@gmail.com>
2026-08-19 10:32:25 -0300

    Update Inteiros.cpp
    Segunda atualização 
    Falta melhorar ele

--- Inteiros.cpp
+++ Inteiros.cpp
@@ -13,35 +13,32 @@ Exercício de inteiros
 
 class INTEIROS
 {
-    private: int digitos,resultado,numero;
+    private: int digitos,resultado,numero,digitopar,digitoimpar,soma,produto;
     
     public: void setNumero(int n)
     {
         numero = n;
-        
-        
     };
     
     public: int getNumero()
     {
-     
         return numero;
     }
     
     public: int NumeroDigitos(int numero)
     {
-       int contador = 0;
+       int digitos = 0;
         
        do {
             numero = numero/10;
-            contador++;
+            digitos++;
             
             
             
-        }  while  (contador != 0);
+        }  while  (numero != 0);
         
         
-       return contador;
+       return digitos;
        
     }
     public: int getNumeroDigitos()
@@ -52,11 +49,86 @@ class INTEIROS
     };
   
     
+
+
+public: int SomaDigitos(int numero)
+{
+    digitopar = 0;
+    digitoimpar = 0;
+    soma = 0;
+    do{
+        digitos = numero % 10;
+        soma = soma + digitos;
+        
+        if(digitos % 2 == 0)
+        {
+            printf("O numero %d é par\n",digitos);
+            digitopar++;
+        }
+        else
+        {
+            printf("O numero %d é impar\n",digitos);
+            digitoimpar++;
+        }
+        numero = numero / 10;
+    }while (numero != 0);
+    
+    return soma;
+};
+ public: int getSomaDigitos()
+    {
+        
+        return soma;
+        
+    };
+public: int ProdutoDigitos(int numero)
+{
+    int produto = 1;
+    
+    do
+    {
+        digitos = numero % 10;
+        produto = produto * digitos;
+        numero = numero / 10;
+        
+    }while(numero != 0);
+    
+    return produto;
+}
+ public: int getProdutoDigitos()
+    {
+        
+        return produto;
+    };
+    
+    
+public: int getDigitoPar()
+{
+    return digitopar;
+}
+
+public: int getDigitoImpar()
+{
+    return digitoimpar;
+}
+
+
+
 };
 
 int main()
 {
-    printf("Hello World");
+    INTEIROS obj;
+
+    obj.setNumero(24680);
+
+    printf("Numero: %d\n", obj.getNumero());
+    printf("Quantidade de digitos: %d\n", obj.NumeroDigitos(obj.getNumero()));
+    printf("Soma dos digitos: %d\n", obj.SomaDigitos(obj.getNumero()));
+    printf("O Produto dos digitos: %d\n", obj.ProdutoDigitos(obj.getNumero()));
+    printf("Quantidade de digitos pares: %d\n", obj.getDigitoPar());
+    printf("Quantidade de digitos impares: %d\n", obj.getDigitoImpar());
+    
 
     return 0;
 }
