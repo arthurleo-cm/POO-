b64f2abe7c7bdabead393ddacf2f13a810ce9143
Arthur <arthurhleocadio@gmail.com>
2026-08-28 22:17:05 -0300

    Create questao7.cpp

---
+++ LISTA_DE_EXERCICIO/questao7.cpp
@@ -0,0 +1,17 @@
+/*
+QUESTÃO 007. Desenvolver uma aplicação de software, utilizando o paradigma de
+orientação a objetos, para implementar operações matemáticas com 2 números reais
+(N1 e N2). Implementar os seguintes métodos:
+ Soma, multiplicação, subtração, divisão.
+ Percentual N2 de N1: N2 % de N1.
+ Retornar o maior.
+ Retornar o menor.
+ Verificar se são iguais.
+ Arredondamento para inteiro.
+ Verificar se número é positivo ou negativo.
+ Status de erro operacional (por exemplo, divisão por zero).
+Desenvolver, pelo menos, uma classe e os métodos pertinentes para atender aos
+requisitos apresentados. Obrigatoriamente incorporar construtor, polimorfismo,
+métodos get e set, e sobreposição de classe: toString [retorno: DEFINIR
+PADRÃO] .
+*/
