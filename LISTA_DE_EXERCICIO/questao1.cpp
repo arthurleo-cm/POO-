73a0ffbcc40ac6f8088140b25743a9200a64f290
Arthur <arthurhleocadio@gmail.com>
2026-08-28 22:13:09 -0300

    Create questao1.cpp

---
+++ LISTA_DE_EXERCICIO/questao1.cpp
@@ -0,0 +1,5 @@
+/*QUESTÃO 001. Desenvolver uma aplicação de software, utilizando o paradigma de
+orientação a objetos, para fornecer a data e hora corrente, do sistema, converter
+hora:minutos:segundos correntes em segundos. Desenvolver, pelo menos, uma
+classe e os métodos pertinentes. Obrigatoriamente incorporar construtor,
+polimorfismo e sobreposição de classe: toString [retorno: DEFINIR PADRÃO] */
