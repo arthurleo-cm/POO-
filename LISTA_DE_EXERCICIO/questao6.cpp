ea6d49abb3710ec1d5315fd59e6e7faf45a99e6e
Arthur <arthurhleocadio@gmail.com>
2026-08-28 22:16:32 -0300

    Create questao6.cpp

---
+++ LISTA_DE_EXERCICIO/questao6.cpp
@@ -0,0 +1,17 @@
+/*
+QUESTÃO 006. Desenvolver uma aplicação de software, utilizando o paradigma de
+orientação a objetos, para manipulação de STRINGs. Implementar os seguintes
+métodos:
+ Cadastrar a String
+ Contar o número de caracteres
+ Contar o número de caracteres numéricos
+ Contar o número de espaços em branco
+ Contar o número de vogais
+ Contar o número de ocorrências de um caracter.
+ Inverter a ordem dos caracteres
+ Verificar se a String é numérica
+Desenvolver, pelo menos, uma classe e os métodos pertinentes para atender aos
+requisitos apresentados. Obrigatoriamente incorporar construtor, polimorfismo,
+métodos get e set, e sobreposição de classe: toString [retorno: DEFINIR
+PADRÃO] .
+*/
