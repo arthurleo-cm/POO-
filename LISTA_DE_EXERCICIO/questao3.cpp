374fbcf1f6a08173a68dd96ac3650ac4ed0bf40c
Arthur <arthurhleocadio@gmail.com>
2026-08-28 22:15:28 -0300

    Create questao3.cpp

---
+++ LISTA_DE_EXERCICIO/questao3.cpp
@@ -0,0 +1,16 @@
+/*
+QUESTÃO 003. Desenvolver uma aplicação de software, utilizando o paradigma de
+orientação a objetos, para efetuar a conversão de unidades de temperatura:
+•De graus Celsius(C) para graus Fahrenheit(F):F=(9×C/5)+32)
+•De graus Fahrenheit(F) para graus Celsius(C):C=(F−32)×+5/9
+•De graus Celsius(C) para graus Kelvin(K):K=C+273.15
+•De graus Kelvin(K) para graus Celsius(C):C=K−273.15
+•De graus Celsius(C) para graus Réaumur(Re):Re=C∗4/5
+•De graus Réaumur(Re) para graus Celsius(C):C=Re∗5/4
+•De graus Kelvin(K) para graus Rankine(R):R=K∗1.8
+•De graus Rankine(R) para graus Kelvin(K):K=R/1.8
+Desenvolver, pelo menos, uma classe e os métodos pertinentes para atender aos
+requisitos apresentados. Obrigatoriamente incorporar construtor, polimorfismo,
+métodos get e set, e sobreposição de classe: toString [retorno: DEFINIR
+PADRÃO] .
+*/
