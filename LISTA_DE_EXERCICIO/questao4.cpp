60a1b2b98d80c4618c0f86d1cfedda8c6ef76ee2
Arthur <arthurhleocadio@gmail.com>
2026-08-28 22:16:01 -0300

    Create questao4.cpp

---
+++ LISTA_DE_EXERCICIO/questao4.cpp
@@ -0,0 +1,5 @@
+/*QUESTÃO 004. Desenvolver uma aplicação de software, utilizando o paradigma de
+orientação a objetos, para calcular o perímetro e área das seguintes figuras
+geométricas: retângulo, triangulo, trapézio, círculo. Obrigatoriamente incorporar
+construtor, polimorfismo, métodos get e set, e sobreposição de classe: toString
+[retorno: DEFINIR PADRÃO] .*/
