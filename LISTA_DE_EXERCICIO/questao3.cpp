b67fd846d3318274f4c8b109f990a04cdd67b30e
Arthur <arthurhleocadio@gmail.com>
2026-09-01 17:01:23 -0300

    Update questao3.cpp

--- LISTA_DE_EXERCICIO/questao3.cpp
+++ LISTA_DE_EXERCICIO/questao3.cpp
@@ -14,3 +14,130 @@ requisitos apresentados. Obrigatoriamente incorporar construtor, polimorfismo,
 métodos get e set, e sobreposição de classe: toString [retorno: DEFINIR
 PADRÃO] .
 */
+#include <string>
+#include <cstdio>
+using namespace std;
+
+class TEMPERATURA
+{
+private:
+    float temperatura;
+
+public:
+
+
+    TEMPERATURA(float t = 0)
+    {
+        temperatura = t;
+    }
+
+
+    public: void setTemperatura(float t)
+    {
+        temperatura = t;
+    }
+
+    public: float getTemperatura()
+    {
+        return temperatura;
+    }
+
+    public: float CelsiusParaFahrenheit()
+    {
+        return (9 * temperatura / 5) + 32;
+    }
+
+   
+    public: float FahrenheitParaCelsius()
+    {
+        return (temperatura - 32) * 5 / 9;
+    }
+
+    public: float CelsiusParaKelvin()
+    {
+        return temperatura + 273.15;
+    }
+
+  
+    public: float KelvinParaCelsius()
+    {
+        return temperatura - 273.15;
+    }
+
+
+    public: float CelsiusParaReaumur()
+    {
+        return temperatura * 4 / 5;
+    }
+
+    
+    public: float ReaumurParaCelsius()
+    {
+        return temperatura * 5 / 4;
+    }
+
+    
+    public: float KelvinParaRankine()
+    {
+        return temperatura * 1.8;
+    }
+
+   
+    public: float RankineParaKelvin()
+    {
+        return temperatura / 1.8;
+    }
+
+  
+    public: virtual string toString()
+    {
+        return "Temperatura = " + to_string(temperatura);
+    }
+};
+
+
+
+class TEMPERATURACELSIUS : public TEMPERATURA
+{
+public:
+
+    TEMPERATURACELSIUS(float t = 0) : TEMPERATURA(t)
+    {
+    }
+
+    public: string toString() override
+    {
+        return "Temperatura em Celsius = " + to_string(getTemperatura()) + " C";
+    }
+};
+
+
+int main()
+{
+    float temperatura;
+
+    TEMPERATURACELSIUS vtemperatura;
+
+    printf("\nCONVERSAO DE UNIDADES DE TEMPERATURA");
+
+    printf("\nDigite a temperatura em Celsius: ");
+    scanf("%f", &temperatura);
+
+    vtemperatura.setTemperatura(temperatura);
+
+    printf("\nTEMPERATURA INFORMADA = %7.2f C",
+           vtemperatura.getTemperatura());
+
+    printf("\nCELSIUS PARA FAHRENHEIT = %7.2f F",
+           vtemperatura.CelsiusParaFahrenheit());
+
+    printf("\nCELSIUS PARA KELVIN = %7.2f K",
+           vtemperatura.CelsiusParaKelvin());
+
+    printf("\nCELSIUS PARA REAUMUR = %7.2f Re",
+           vtemperatura.CelsiusParaReaumur());
+
+    printf("\n");
+
+    return 0;
+}
